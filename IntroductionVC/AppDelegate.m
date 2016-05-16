6c5fa12bc7f7ff0c4c15ba78692069b9bc200ea6
jhtxch <jhtxchenhao@163.com>
2016-05-16 15:56:47 +0800

    test branch

--- IntroductionVC/AppDelegate.m
+++ IntroductionVC/AppDelegate.m
@@ -44,13 +44,8 @@
 
 - (void)introductionScrollWithContentOffset:(CGFloat)offset
 {
-    
+//test
 }
-
-
-
-
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
